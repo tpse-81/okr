d150c7e8459bc8a291fe30e5a0b2385284ababc7
Bnyro <bnyro@tutanota.com>
2025-11-16 20:13:03 +0100

    docs: add method documentation to projects API

--- app.py
+++ app.py
@@ -12,36 +12,35 @@ from litestar.plugins.sqlalchemy import AsyncSessionConfig, SQLAlchemyAsyncConfi
 
 import uuid
 
-session_config = AsyncSessionConfig(expire_on_commit=False)
-sqlalchemy_config = SQLAlchemyAsyncConfig(
-    connection_string="sqlite+aiosqlite:///test.sqlite", session_config=session_config, create_all=True
-)  # Create 'async_session' dependency.
 
-async def on_startup(app: Litestar) -> None:
-    # async with sqlalchemy_config.get_session() as session:
-    #     statement = select(func.count()).select_from(Project)
-    #     count = await session.execute(statement)
-
-
-@get("/")
+@get("/hello")
 async def hello_world(db_session: AsyncSession, db_engine: AsyncEngine) -> dict[str, str]:
     """
-        Prints hello world.
+    Prints hello world.
 
-        return: a JSON object
+    return: a JSON object
     """
     return {"hello": "world"}
 
 
-@get("/peter", media_type="text/html")
-async def hello_peter() -> str:
+@get("/", media_type="text/html")
+async def main_page() -> str:
+    """
+    Renders the main HTML page.
+
+    return: a raw HTML string
+    """
     with open("main.html", "r") as f:
-        # return {"name": "peter"}
         return f.read()
 
 
 @get("/projects")
 async def get_projects(db_session: AsyncSession, db_engine: AsyncEngine) -> list[Project]:
+    """
+    Get the list of projects.
+
+    return: a JSON list of projects
+    """
     return list(await db_session.scalars(select(Project)))
 
 
@@ -52,9 +51,19 @@ async def create_project(
     name: str | None = Parameter(default=None),
     deadline: int | None = Parameter(default=None),
     creation_date: int | None = Parameter(default=None),
-) -> str:
+) -> dict[str, str]:
+    """
+    Create a new project.
+
+    param name: the name of the project to create
+    param deadline: the deadline as UNIX timestamp
+    param creation_date: the creation_date as UNIX timestamp
+    return: whether the project was successfully created
+    """
+
+    # all project parameters are mandatory, so enforce they're not unset
     if name is None or deadline is None or creation_date is None:
-        return "Fehlerhafte Anfrage"
+        return {"message": "Failed to create project"}
 
     # randomly generate a project id
     project_id = uuid.uuid4()
@@ -62,22 +71,32 @@ async def create_project(
 
     # create new database entry for project with parameters from URL
     db_session.add(project)
-
     await db_session.commit()
 
-    return "Projekt erstellt"
+    return {"message": "Project created"}
 
 
 @get("/images/{filename:str}")
-async def serve_image(filename: str) -> File:
+async def serve_images(filename: str) -> File:
+    """
+    Serve the static image for the given filename
+
+    returns: the binary file as byte stream
+    """
     return File(
         path=Path("images") / filename,
     )
 
 
+# Create a session config that is linked to an SQLite database.
+session_config = AsyncSessionConfig(expire_on_commit=False)
+sqlalchemy_config = SQLAlchemyAsyncConfig(
+    connection_string="sqlite+aiosqlite:///test.sqlite", session_config=session_config, create_all=True
+)
+
+# Run the web app
 app = Litestar(
-    route_handlers=[hello_world, hello_peter, serve_image, get_projects, create_project],
-    on_startup=[on_startup],
+    route_handlers=[hello_world, main_page, serve_images, get_projects, create_project],
     debug=True,
     plugins=[SQLAlchemyPlugin(config=sqlalchemy_config)],
 )
--- models/project.py
+++ models/project.py
@@ -6,8 +6,14 @@ from uuid import UUID
 
 @dataclass
 class Project(base.UUIDBase):
+    """
+    Data model of an OKR project.
+    """
+
     __tablename__ = "projects"
+    # primary key
     id: Mapped[UUID]
+    # Table columns/attributes
     name: Mapped[str]
     creation_date: Mapped[int]
     deadline: Mapped[int]
