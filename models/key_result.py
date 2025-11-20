8ee3676fa537eb9bd91ac74d91730f2f0d116711
Kaan Elman <k.elman@hotmail.de>
2025-11-20 13:29:27 +0100

    feat: add key results

---
+++ models/key_result.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from litestar.plugins.sqlalchemy import base
+from sqlalchemy.orm import Mapped
+from uuid import UUID
+
+
+@dataclass
+class KeyResult(base.UUIDBase):
+    """
+    Data model of an OKR key result.
+    """
+
+    __tablename__ = "key_results"
+    # primary key
+    id: Mapped[UUID]
+    # Table columns/attributes
+    project_id: Mapped[int]
+    objective_id: Mapped[int]
+    description: Mapped[str]
+    start_value: Mapped[float]
+    end_value: Mapped[float]
