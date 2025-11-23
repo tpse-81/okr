71cbbb889e5f12faaad3dec3a57595be6fd7ee31
Javes64 <yavuzsenguel64@gmail.com>
2025-11-23 18:32:00 +0100

    feat: add project tests

---
+++ test_projects.py
@@ -0,0 +1,29 @@
+
+from litestar.status_codes import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from litestar.testing import TestClient
+
+from app import app
+app.debug = True
+
+
+def test_project_check():
+    with TestClient(app=app) as client:
+        response = client.get("/projects/create?name=name&deadline=5&creation_date=10")
+        assert response.status_code == HTTP_200_OK
+        assert response.json() == {'message': 'successfully created project'}
+
+        response = client.get("/projects")
+        assert response.status_code == HTTP_200_OK
+        t = response.json()[0]
+        assert t["creation_date"] == 10
+        assert t["name"] == "name"
+        assert t["deadline"] == 5
+
+def test_empty_project_check():
+    with TestClient(app=app) as client:
+        response = client.get("/projects/create?name=name&deadline=5&creation_date=")
+        assert response.status_code == HTTP_400_BAD_REQUEST
+
+        response = client.get("/projects")
+        assert response.status_code == HTTP_200_OK
+        assert len(response.json()) == 0
