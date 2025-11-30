1a7bb2fab0586b647823432dce2bc31ae08ae152
Kaan Elman <k.elman@hotmail.de>
2025-11-30 18:27:55 +0100

    feat: many-to-many relationship between objective and project

---
+++ models/project_objective.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Table
+from advanced_alchemy.base import orm_registry
+
+
+# association table for linking projects and objectives (many-to-many relationship)
+# where the combination of project_id and objective_id is unique
+project_objective = Table(
+    "project_objective",
+    orm_registry.metadata,
+
+    # column project_id contains the UUID of the project
+    Column("project_id", ForeignKey("projects.id", ondelete="CASCADE"), primary_key=True),
+
+    # column objective_id contains the UUID of the objective
+    Column("objective_id", ForeignKey("objectives.id", ondelete="CASCADE"), primary_key=True)
+)
