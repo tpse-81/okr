83f54d39370606a505c6de9878a0bdd9656cfe3b
hasi73jac <116826480+hasi73jac@users.noreply.github.com>
2026-02-14 22:12:38 +0100

    refactor: include archive state information in projects responses

--- dto/read_dto.py
+++ dto/read_dto.py
@@ -13,7 +13,7 @@ class ProjectReadDTO(SQLAlchemyDTO[Project]):
     DTO used for serializing Project models for read operations (limits output and prevents recursive relationships)
     """
 
-    config = DTOConfig(exclude={"is_archived", "archive_reason", "objectives"})
+    config = DTOConfig(exclude={"objectives"})
 
 
 class ObjectiveReadDTO(SQLAlchemyDTO[Objective]):
@@ -21,7 +21,7 @@ class ObjectiveReadDTO(SQLAlchemyDTO[Objective]):
     DTO used for serializing Objective models for read operations (limits output and prevents recursive relationships)
     """
 
-    config = DTOConfig(exclude={"is_archived", "parent_id", "children", "key_results"})
+    config = DTOConfig(exclude={"parent_id", "children", "key_results"})
 
 
 class KeyResultReadDTO(SQLAlchemyDTO[KeyResult]):
