2134dd95b848f82022cf568e36baffb652dfe614
Javes64 <yavuzsenguel64@gmail.com>
2026-03-01 12:07:17 +0100

    fix:  added dataclasses for the permission returns

--- app.py
+++ app.py
@@ -842,20 +842,31 @@ async def update_key_result_current_value(
     return key_result
 
 
+@dataclass
+class ProjectPermissionsResponse:
+    can_lead: bool
+    can_write: bool
+
+
+@dataclass
+class PermissionsResponse:
+    can_write: bool
+
+
 @get("/projects/{project_id:str}/permissions")
 async def get_project_permissions(
     db_session: AsyncSession,
     request: Request,
     project_id: str = Parameter(),
-) -> dict[str, bool]:
-    return {
-        "can_lead": await has_project_lead_permissions(
+) -> ProjectPermissionsResponse:
+    return ProjectPermissionsResponse(
+        can_lead=await has_project_lead_permissions(
             db_session, request.user, project_id
         ),
-        "can_write": await has_weak_project_permissions(
+        can_write=await has_weak_project_permissions(
             db_session, request.user, project_id
         ),
-    }
+    )
 
 
 @get("/objectives/{objective_id:str}/permissions")
@@ -863,12 +874,12 @@ async def get_objective_permissions(
     db_session: AsyncSession,
     request: Request,
     objective_id: str = Parameter(),
-) -> dict[str, bool]:
-    return {
-        "can_write": await has_objective_write_permissions(
+) -> PermissionsResponse:
+    return PermissionsResponse(
+        can_write=await has_objective_write_permissions(
             db_session, request.user, objective_id
         ),
-    }
+    )
 
 
 @get("/key_results/{key_result_id:str}/permissions")
@@ -876,12 +887,12 @@ async def get_key_result_permissions(
     db_session: AsyncSession,
     request: Request,
     key_result_id: str = Parameter(),
-) -> dict[str, bool]:
-    return {
-        "can_write": await has_key_result_write_permissions(
+) -> PermissionsResponse:
+    return PermissionsResponse(
+        can_write=await has_key_result_write_permissions(
             db_session, request.user, key_result_id
         ),
-    }
+    )
 
 
 @get("/tasks/{task_id:str}/permissions")
@@ -889,12 +900,10 @@ async def get_task_permissions(
     db_session: AsyncSession,
     request: Request,
     task_id: str = Parameter(),
-) -> dict[str, bool]:
-    return {
-        "can_write": await has_task_write_permissions(
-            db_session, request.user, task_id
-        ),
-    }
+) -> PermissionsResponse:
+    return PermissionsResponse(
+        can_write=await has_task_write_permissions(db_session, request.user, task_id),
+    )
 
 
 @post("/projects/{project_id:str}/objectives", dto=ObjectiveWriteDTO, return_dto=None)
